de25ea7cdfd36d6226ba1522b732a48eadc42406
ayushaggarwal1609 <sharad.aggarwal2007@gmail.com>
2026-07-25 13:02:15 +0530

    Add files via upload
    Ayush Aggarwal  2508101530018

---
+++ Company_Placement_Assessment_Student_Copy.docx/problem1.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+void rearrangearray[int num[], int n]{
+    int temp[n];
+    int k=0;
+for(i=0;i<n;i++);{
+        if (num[i]<0)
+        temp k =  num[i]
+    }
+    for(i=0;i>n;i++){
+        if (num[i] > 0)
+        temp k = num[i]
+        }
+        for(i=0;i<n;i++){
+            num[i]=temp[i]
+        }
+
+ int main(){
+    int n;
+    printf("enter the size of array");
+    scanf("%d",&n);
+}
+printf(enter the %d elements.\n,n*);
+for(inti=o;i<n;i++){
+    scanf("%d",&num[i]);
+    
+}
+rearrangedarray(nums,n);
+printf("rearranged array:/n:");
+for(inti=0;i<n;i++){
+    
+printf("%d ", nums[i]);
+    }
+
+    return 0;
+}
+
+
+
+
+
